daa7f12510eaf0ba9904078db6dba07aa260ed69
MadhaviRockandla <rokandla@gmail.com>
2022-03-11 17:33:54 -0500

    changed idea/misc

---
+++ src/main/java/hammurabi/Hammurabi.java
@@ -0,0 +1,16 @@
+package hammurabi;
+
+public class Hammurabi {
+    Random
+}
+    public static void main(String[] args) {
+        new Hammurabi().run();
+}
+
+    public void playgame (){
+        int population;
+        int grain;
+        int acres;
+    }
+
+    public int askHowManyAcresToBuy(int price, int bushels){ return 0;}
