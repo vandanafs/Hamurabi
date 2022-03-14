4680aa05e079584e93734f6ef43bdbeb51918a87
Madhavi Rockandla <99415835+MadhaviRockandla@users.noreply.github.com>
2022-03-13 20:04:52 -0400

    solved commits

--- src/main/java/hammurabi/Hammurabi.java
+++ src/main/java/hammurabi/Hammurabi.java
@@ -1,21 +1,4 @@
 package hammurabi;
-
-<<<<<<< HEAD
-public class Hammurabi {
-    Random
-}
-    public static void main(String[] args) {
-        new Hammurabi().run();
-}
-
-    public void playgame (){
-        int population;
-        int grain;
-        int acres;
-    }
-
-    public int askHowManyAcresToBuy(int price, int bushels){ return 0;}
-=======
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
@@ -29,4 +12,4 @@ public class Hammurabi {
 
     }
 }
->>>>>>> a12895d2d0dcf34962e2ebc34c0ec1f785ef213c
+
